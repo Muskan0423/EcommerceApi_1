345c3855d15d6a953151d276351b4fd42c792238
Subham Das <dsubham257.sd@gmail.com>
2024-10-10 21:46:58 +0530

    aaded filterComp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,63 +1,59 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addToCart } from '../slices/cartslice';
-import ProductUpdateButtons from './ProductUpdateButtons';
-import ButtonPlusMinus from "./ButtonPlusMinus"
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../slices/cartslice";
+import ProductUpdateButtons from "./ProductUpdateButtons";
+import ButtonPlusMinus from "./ButtonPlusMinus";
+import { Link } from "react-router-dom";
 
 const Card = ({ item }) => {
-
-  const dispatch = useDispatch()
-  const {cartData} = useSelector((state)=>state.cart)
-
+  const dispatch = useDispatch();
+  const { cartData } = useSelector((state) => state.cart);
   const data = cartData.find((elem) => elem._id == item._id);
-
-
-  
-
-  const { currentCurrencyName, currentCurrencyPrice } = useSelector((state) => state.currency);
+  const { currentCurrencyName, currentCurrencyPrice } = useSelector(
+    (state) => state.currency
+  );
 
   return (
     <div className="w-96 ml-2 flex flex-col items-center bg-white shadow-md rounded-xl transition-transform duration-500 hover:scale-105 hover:shadow-xl">
       <Link to={`/products/${item._id}`} className="w-full">
-        <img 
-          src={item.url}
-          alt="Product" 
-          className="h-96 w-96 rounded-t-xl" 
-        />   </Link>
-        <div className="px-4 py-3 w-full">
-          <span className="text-gray-700 mr-3 uppercase text-xs">{item.title}</span>
-          <p className="text-lg font-bold text-black truncate block capitalize">{item.category}</p>
-          <p className="text-lg font-semibold text-black">
+        <img src={item.url} alt="Product" className="h-96 w-96 rounded-t-xl" />{" "}
+      </Link>
+      <div className="px-4 py-3 w-full">
+        <span className="text-gray-700 mr-3 uppercase text-xs">
+          {item.title}
+        </span>
+        <p className="text-lg font-bold text-black truncate block capitalize">
+          {item.category}
+        </p>
+        <p className="text-lg font-semibold text-black">
           {item.quantity
-          ? (item.price * item.quantity * currentCurrencyPrice).toFixed(2)
-          : (item.price * currentCurrencyPrice).toFixed(2)}{" "}
-            <span className="px-2 text-red-400">({currentCurrencyName})</span>
-          </p>
-          {item.quantity ? (
-        <ButtonPlusMinus item={item} />
-      ) : data ? (
-        <button className="px-6 py-2 text-white bg-red-900 rounded" disabled>
-          Added into Cart
-        </button>
-      ) : (
-        <button
-          className="px-6 py-2 text-white bg-slate-700 rounded hover:bg-slate-900"
-          onClick={() => dispatch(addToCart(item))}
-        >
-          Add To Cart
-        </button>
-      )}
-          <div className="flex items-center">
-            <p className="text-lg font-semibold text-black cursor-auto my-3"></p>
-            <del>
-              <p className="text-sm text-gray-600 cursor-auto ml-2"></p>
-            </del>
-          </div>
+            ? (item.price * item.quantity * currentCurrencyPrice).toFixed(2)
+            : (item.price * currentCurrencyPrice).toFixed(2)}{" "}
+          <span className="px-2 text-red-400">({currentCurrencyName})</span>
+        </p>
+        {item.quantity ? (
+          <ButtonPlusMinus item={item} />
+        ) : data ? (
+          <button className="px-6 py-2 text-white bg-red-900 rounded" disabled>
+            Added into Cart
+          </button>
+        ) : (
+          <button
+            className="px-6 py-2 text-white bg-slate-700 rounded hover:bg-slate-900"
+            onClick={() => dispatch(addToCart(item))}
+          >
+            Add To Cart
+          </button>
+        )}
+        <div className="flex items-center">
+          <p className="text-lg font-semibold text-black cursor-auto my-3"></p>
+          <del>
+            <p className="text-sm text-gray-600 cursor-auto ml-2"></p>
+          </del>
         </div>
-   
+      </div>
     </div>
   );
-}
+};
 
 export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <RouterProvider router={router} />
-    </Provider>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
